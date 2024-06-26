6911a20e1757104727e6629fb2546d0b45a85142
Vlad <vlad63mazin@gmail.com>
2024-06-26 15:12:46 +0400

    add market

---
+++ myproject/marketapp/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+'''
+Создайте три модели Django: клиент, товар и заказ.
+Клиент может иметь несколько заказов. Заказ может содержать несколько товаров. Товар может входить в несколько заказов.
+
+Поля модели «Клиент»:
+— имя клиента
+— электронная почта клиента
+— номер телефона клиента
+— адрес клиента
+— дата регистрации клиента
+
+Поля модели «Товар»:
+— название товара
+— описание товара
+— цена товара
+— количество товара
+— дата добавления товара
+
+Поля модели «Заказ»:
+— связь с моделью «Клиент», указывает на клиента, сделавшего заказ
+— связь с моделью «Товар», указывает на товары, входящие в заказ
+— общая сумма заказа
+— дата оформления заказа
+'''
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    adr = models.CharField(max_length=100)
+    date_add = models.DateField(auto_now_add=True)
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    count = models.IntegerField(default=0)
+    date_add = models.DateField(auto_now_add=True)
+
+class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    date_add = models.DateField(auto_now_add=True)
--- myproject/marketapp/urls.py
+++ myproject/marketapp/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('game/', include('gameapp.urls')),
-    path('info/', include('infoapp.urls')),
-    path('market/', include('marketapp.urls'))
+    path('', index, name='index')
 ]
